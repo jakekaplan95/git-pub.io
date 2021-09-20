d4e4b67a96d6a2bd8df6536af87a836b4780cb76
Jake Kaplan <kaplanjt@gmail.com>
2021-09-20 13:00:36 -0700

    Allows for the /drinks route

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+//Express Library
 const express = require('express');
 
+//App creation
 const app = express();
 const port = 3000;
 
+//Drinks additon from models
+const drinks = require("./models/drinks")
 
+//Initial first page
 app.get('/', (req, res) => {
     res.send("Welcome to the Gitpub App!")
 });
 
+//Drinks route
+app.get('/drinks', (req, res) =>{
+    res.send(drinks);
+});
 
 
+//Listener
 app.listen(3000, () => {
     console.log("listening");
 });
