ca3a40a69bdf589f9c7e5cd267c76b2bfeb2df15
Jake Kaplan <kaplanjt@gmail.com>
2021-09-21 19:21:41 -0700

    This commit has the foods added to the index, with full functionality

--- server.js
+++ server.js
@@ -21,16 +21,21 @@ app.get('/drinks', (req, res) =>{
 });
 
 
-// This is the code that I cannot get to work , linking the foods and drinks in the same index
-// app.get('/food', (req, res) =>{
-//     res.render("food_index.ejs", {food: food})
-//     res.render("drinks_index.ejs", {drinks, drinks} );
-// });
+
+app.get('/food', (req, res) =>{
+    res.render("food_index.ejs", {food: food, drinks: drinks});
+});
 
 //Link to Specific Drinks
 app.get('/drinks/:drinksIndex', (req, res) => {
     const drink = drinks[req.params.drinksIndex]
     res.render('drinks_show.ejs', {drink: drink});
+});
+
+//Link to Specific Foods
+app.get('/food/:foodIndex', (req, res) => {
+    const foods = food[req.params.foodIndex]
+    res.render('food_show.ejs', {foods: foods});
 });
 
 
