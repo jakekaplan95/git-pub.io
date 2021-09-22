be3520692061686aac17190e931476852b99a07f
Jake Kaplan <kaplanjt@gmail.com>
2021-09-21 19:07:58 -0700

    This commit has a functioning drink server and I am still trying to work on getting the drinks and foods on the same page under food_index.ejs

--- server.js
+++ server.js
@@ -5,18 +5,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//Drinks additon from models
-const drinks = require("./models/drinks")
+//Drinks and food additon from models
+const drinks = require("./models/drinks");
+const food = require("./models/food");
 
 //Initial first page
 app.get('/', (req, res) => {
-    res.send("Welcome to the Gitpub App!")
+    res.send("Welcome to the Gitpub App!");
 });
 
 //Drinks route
 app.get('/drinks', (req, res) =>{
     res.render("drinks_index.ejs", {drinks: drinks
     });
+});
+
+
+// This is the code that I cannot get to work , linking the foods and drinks in the same index
+// app.get('/food', (req, res) =>{
+//     res.render("food_index.ejs", {food: food})
+//     res.render("drinks_index.ejs", {drinks, drinks} );
+// });
+
+//Link to Specific Drinks
+app.get('/drinks/:drinksIndex', (req, res) => {
+    const drink = drinks[req.params.drinksIndex]
+    res.render('drinks_show.ejs', {drink: drink});
 });
 
 
